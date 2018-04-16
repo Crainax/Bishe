080bf7867d6846f5cb4165905fac1f801eb55aa7
Crainax <413997411@qq.com>
2018-04-16 22:00:35 +0800

    初始化

---
+++ resource/get_query_url.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+# 关闭https证书验证警告
+requests.packages.urllib3.disable_warnings()
+
+# 城市名代码查询字典
+# key：城市名 value：城市代码
+from .stations import stations_dict
+
+# 反转k，v形成新的字典
+code_dict = {v: k for k, v in stations_dict.items()}
+
+
+def get_query_url(text):
+    '''
+    返回调用api的url链接
+    '''
+    # 解析参数 aggs[0]里是固定字符串：车票查询 用于匹配公众号接口
+    args = str(text).split(' ')
+    try:
+        date = args[1]
+        from_station_name = args[2]
+        to_station_name = args[3]
+        from_station = stations_dict[from_station_name]
+        to_station = stations_dict[to_station_name]
+    except:
+        date, from_station, to_station = '--', '--', '--'
+        # 将城市名转换为城市代码
+
+    # api url 构造
+    url = (
+        'https://kyfw.12306.cn/otn/leftTicket/query?'
+        'leftTicketDTO.train_date={}&'
+        'leftTicketDTO.from_station={}&'
+        'leftTicketDTO.to_station={}&'
+        'purpose_codes=ADULT'
+    ).format(date, from_station, to_station)
+    print(url)
+
+    return url
--- resource/parse_station.py
+++ resource/parse_station.py
@@ -6,6 +6,7 @@ import requests
 import re
 
 #关闭https证书验证警告
+
 requests.packages.urllib3.disable_warnings()
 # 12306的城市名和城市代码js文件url
 url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9018'
@@ -14,3 +15,7 @@ pattern = u'([\u4e00-\u9fa5]+)\|([A-Z]+)'
 result = re.findall(pattern,r.text)
 station = dict(result)
 print(station)
+f = open('station.py', 'w')
+print(station , file=f)
+f.close()
+
